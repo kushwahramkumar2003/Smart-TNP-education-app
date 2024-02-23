d3c318358e3f5f90861c1415ee8002b26953b066
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2024-02-23 14:04:20 +0530

    Update code formatting and fix minor bugs

---
+++ apps/api/src/index.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${3000}`);
+});
