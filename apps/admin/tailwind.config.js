c43d478c69f7d51c599b53281e589fa566191808
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2024-03-16 19:31:22 +0530

    shadcn added and admin added

---
+++ apps/admin/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+import ("@repo/ui/tailwind.config")
+export default {
+
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
