ded4c0e00a3c935575b87345128f6935a9601cd9
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2024-04-26 18:21:35 +0530

    LiveSchedule page added

--- apps/admin/src/pages/LiveSchedule/LiveSchedule.tsx
+++ apps/admin/src/pages/LiveSchedule/LiveSchedule.tsx
@@ -1,5 +1,9 @@
 const LiveSchedule = () => {
-  return <div>LiveSchedule</div>;
+  return (
+    <div className="mt-3 flex flex-col">
+      <h1 className="text-2xl font-semibold ">Live Schedule</h1>
+    </div>
+  );
 };
 
 export default LiveSchedule;
