ea2941535d183e31d83a955644634ea5caf81e7d
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2024-04-27 21:41:24 +0530

    calender component added

--- apps/admin/src/pages/LiveSchedule/LiveSchedule.tsx
+++ apps/admin/src/pages/LiveSchedule/LiveSchedule.tsx
@@ -1,7 +1,7 @@
 const LiveSchedule = () => {
   return (
     <div className="mt-3 flex flex-col">
-      <h1 className="text-2xl font-semibold ">Live Schedule</h1>
+      <h1 className="text-2xl font-semibold ">Live Event Schedule</h1>
     </div>
   );
 };
