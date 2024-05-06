a3588aa2f8ddb860f3b75e0c6ce69c32e80c4815
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-05-06 23:22:55 +0530

    bug fixes

--- apps/api/src/routes/admin/teacher.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { generateTeacherRegeToken } from "../../controllers/teacherManagmentControllers";
-
-const router = Router();
-
-router.post("/genToken", generateTeacherRegeToken);
-
-export default router;
--- apps/api/src/utils/stringToArr.ts
+++
@@ -1,3 +0,0 @@
-export const strToArr = (str: string): string[] => {
-  return str.split(", ").map((item) => item.trim());
-};
