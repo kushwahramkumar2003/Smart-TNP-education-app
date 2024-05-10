79ea6cd21e6786778e0e2bd006bf6dcf016876fc
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-05-10 07:48:38 +0530

    live schedule

---
+++ apps/web/src/pages/LiveShcedule/LiveSchedule.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function LiveSchedule() {
+  return (
+    <div>
+      <div>
+        <h1 className='py-3 font-semibold text-2xl'>Live Event Schedule</h1>
+        <div className='flex flex-row justify-between'>
+        <div>
+        <div>Calender</div>
+        <div>Upcoming</div>
+        </div>
+        <div>Today</div>
+        </div>
+        </div>
+    </div>
+  );
+}
