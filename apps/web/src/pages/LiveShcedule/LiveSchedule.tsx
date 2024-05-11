32807cc40a3c66a05997e3b35f771ca84c8a2e96
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-05-11 22:06:43 +0530

    add component ui

--- apps/web/src/pages/LiveShcedule/LiveSchedule.tsx
+++ apps/web/src/pages/LiveShcedule/LiveSchedule.tsx
@@ -1,16 +1,25 @@
+import Calendar from '@/components/CalenderComponents/Calender';
 import React from 'react';
 
 export default function LiveSchedule() {
   return (
     <div>
-      <div>
-        <h1 className='py-3 font-semibold text-2xl'>Live Event Schedule</h1>
+      <div className='py-12'>
+        <h1 className='py-3  font-semibold text-2xl'>Live Event Schedule</h1>
         <div className='flex flex-row justify-between'>
         <div>
-        <div>Calender</div>
-        <div>Upcoming</div>
+        <div className='max-h-fit'>
+        <Calendar></Calendar>
         </div>
-        <div>Today</div>
+        <div className='w-1/2 bg-white'>Upcoming</div>
+        <div>
+
+        </div>
+        </div>
+        <div className='w-1/2 bg-white'>Today</div>
+        <div>
+          
+        </div>
         </div>
         </div>
     </div>
