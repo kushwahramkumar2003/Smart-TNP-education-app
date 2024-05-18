705b5277ebfda98d9fd6bc6bbf1b133f5c0f2685
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2024-05-18 21:10:44 +0530

    video call controllers and routes added

--- apps/api/src/config/index.ts
+++ apps/api/src/config/index.ts
@@ -9,6 +9,9 @@ const config = {
     process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY,
   port: process.env.PORT || 8080,
   jwtSecret: process.env.JWT_SECRET || "secret",
+  LIVEKIT_URL: process.env.LIVEKIT_URL || "",
+  LIVEKIT_API_KEY: process.env.LIVEKIT_API_KEY || "",
+  LIVEKIT_API_SECRET: process.env.LIVEKIT_API_SECRET || "",
 };
 
 export default config;
--- apps/api/src/routes/admin/index.ts
+++ apps/api/src/routes/admin/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import teacherRoutes from "./teacher.routes";
 import courseRoutes from "./course.routes";
+import classRoutes from "./liveClass.routes";
 
 const router = Router();
 
 router.use("/teachers", teacherRoutes);
 router.use("/course", courseRoutes);
+router.use("/class", classRoutes);
 
 export default router;
